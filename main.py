eb0b211aba3ef785c7c386b1f3b9903cb63ec728
Sisu <sisu.bankroll839@passmail.net>
2025-10-03 17:52:51 -0600

    Asteroids: Draw Player

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -7,19 +9,28 @@ def main():
     print("Screen height: " + str(SCREEN_HEIGHT))
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
+    # Create the play object outside the game loop
+    player = Player(SCREEN_HEIGHT / 2, SCREEN_WIDTH /2)
     while True:
-        screen.fill((0,0,0))
-        pygame.display.flip()
+        
+        screen.fill((0,0,0)) # Clear the creen black
+        player.draw(screen)
+        pygame.display.flip() # update the display
+        dt = clock.tick(60) / 1000 # Delta Time in seconds
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                    
-                    
+                
                     
 if __name__ == "__main__":
     main()
+
+
+
 
 
 
